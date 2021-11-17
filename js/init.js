0ba04e6a0df55799e0e7c05985dd58719a0f2f4e
web2019600596 <web2019600596@gmail.com>
2021-11-17 10:18:01 -0600

    Carga

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCNcFext8pP7BP5eS1ekX4FskFAttBko18",
+  authDomain: "web2019600596-fa343.firebaseapp.com",
+  projectId: "web2019600596-fa343",
+  storageBucket: "web2019600596-fa343.appspot.com",
+  messagingSenderId: "396990033297",
+  appId: "1:396990033297:web:f79baede0aeb90499ebe30",
+  measurementId: "G-L00CQNND9M"
+    });
